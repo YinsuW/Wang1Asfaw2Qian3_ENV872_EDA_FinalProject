b11ea8173470c64e99e93a051eb250a991f9cb59
YinsuW <97139108+YinsuW@users.noreply.github.com>
2022-04-15 10:43:35 -0400

    updated analysis codes

--- Data Analysis.R
+++ Data Analysis.R
@@ -1,35 +1,23 @@
 ## Question 1: Is physical activity associated with the level of C-protein? If so, what is the exact relationship? Is it dose-response? 
 
-#method 1
-# Split the data into training and test set
-set.seed(123)
-training.samples <- PA.c.protein$LBXCRP %>%
-  createDataPartition(p = 0.8, list = FALSE)
-train.data  <- PA.c.protein[training.samples, ]
-test.data <- PA.c.protein[-training.samples, ]
-# Build the model
-model <- lm(LBXCRP ~ poly(PA, 8, raw = TRUE), data = train.data)
-# Make predictions
-predictions <- model %>% predict(test.data)
-# Model performance
-modelPerfomance<-data.frame(RMSE = RMSE(predictions, test.data$LBXCRP),
-                            R2 = R2(predictions, test.data$LBXCRP))
+```{r echo=FALSE, message=FALSE, warning=FALSE, fig.cap="C-protein Level under Different Physical Activity Status (Log-transformed)"}
+PA.protein.lm.1<-lm(LBXCRP ~ PA,data = PA.c.protein)
+PA.protein.lm.2<-lm(LBXCRP ~ PA+RIDAGEYR+RIAGENDR,data = PA.c.protein)
+PA.protein.lm.3<-lm(LBXCRP ~ PA+RIDAGEYR+RIAGENDR+LBDHDDSI+LBDTCSI+SBP+SMQ040+DIQ010+RIDRETH1+DMDEDUC2+INDFMPIR+BMXBMI,data = PA.c.protein)
+ggplot(PA.c.protein,aes(y=LBXCRP,x=PA))+
+  geom_point()+
+  geom_smooth(method  = "lm")+
+  labs(x='Physical Activity (MET*Mins)',y="C-protein Level (mg/dL)")
+```
 
-summary(lm(LBXCRP ~ poly(PA, 8, raw = TRUE), data = train.data))
-print(modelPerfomance)
-
-ggplot(train.data, aes(PA, LBXCRP) ) +
-  geom_point() +
-  stat_smooth(method = lm, formula = y ~ poly(x, 8, raw = TRUE))
-
-#method 2
-mod_lm <- gam(LBXCRP ~ PA, data=PA.c.protein)
-summary(mod_lm)
+## Question 2: Are different levels of framingham risk scores the effect modifiers for this relationship between physical activity and C-protein? How do they modify the relationship?
 
 
-## Question 2: Are different levels of framingham scores the effect modifiers for this relationship between physical activity and C-protein? How do they modify the relationship?
-
-mod_gam3 <- gam(LBXCRP ~ te(PA, risk), data=PA.c.protein)
-summary(mod_gam3)
-vis.gam(mod_gam3, type='response', plot.type='persp',theta=30, n.grid=500, border=NA)
-visreg2d(mod_gam3, xvar='risk', yvar='PA', scale='response')
+```{r echo=FALSE, message=FALSE, warning=FALSE,fig.cap="C-protein Level under Differen PA Status by Different Framingham Risk Scores (Log-tranformed)"}
+PA.protein.interaction<-lm(LBXCRP ~ PA+PA*risk_level,data = PA.c.protein)
+PA.protein.interaction<-lm(LBXCRP ~ PA++RIDAGEYR+RIAGENDR+PA*risk_level,data = PA.c.protein)
+ggplot(PA.c.protein,aes(y=LBXCRP,x=PA,color=risk_level))+
+  geom_point()+
+  geom_smooth(method  = "lm")+
+  labs(x='Physical Activity (MET*Mins)',y="C-protein Level (mg/dL)",color="Framingham Risk Score Levels")
+```
